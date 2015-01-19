b20e2f111a76b99a2e798eeb80b60b57b4e77a68
ludovic.launer@continuent.com <ludovic.launer@continuent.com@9f5e3340-5c9a-25cb-7c1c-c133a2e9e928>
2015-01-19 15:34:16 +0000

    CONT-93 Branch for Windows

--- commons/src/java/com/continuent/tungsten/common/cluster/resource/notification/ClusterStateTransitionPhase.java
+++
@@ -1,7 +0,0 @@
-
-package com.continuent.tungsten.common.cluster.resource.notification;
-
-public enum ClusterStateTransitionPhase
-{
-    none, begin, processing, abort, commit, undefined
-}
--- commons/src/java/com/continuent/tungsten/common/tdf/dto/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Data Transfer Object
- * Provides the classes necessary to transfer data to the TDF.
- * 
- * @author <a href="mailto:ludovic.launer@continuent.com">Ludovic Launer</a>
- * @version 1.0
- */
-
-package com.continuent.tungsten.common.tdf.dto;

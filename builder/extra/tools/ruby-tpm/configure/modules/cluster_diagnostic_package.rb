b9547866e5ba79c6c314b047b5dc5093dc5ec657
neil.armitage@continuent.com <neil.armitage@continuent.com@9f5e3340-5c9a-25cb-7c1c-c133a2e9e928>
2015-02-16 09:31:03 +0000

    add more machine info to package

--- builder/extra/tools/ruby-tpm/configure/modules/cluster_diagnostic_package.rb
+++ builder/extra/tools/ruby-tpm/configure/modules/cluster_diagnostic_package.rb
@@ -1,15 +1,15 @@
 module ClusterDiagnosticPackage
   LOG_SIZE = 10*1024*1024
-  
+
   def get_diagnostic_file
     @zip_file
   end
-  
+
   def parsed_options?(arguments)
     remainder = super(arguments)
-    
+
     @zip_file = nil
-    
+
     return remainder
   end
 
@@ -57,10 +57,10 @@ module ClusterDiagnosticPackage
       end
     end
   end
-  
+
   def commit
     super()
-    
+
     begin
       diag_dir = "#{ENV['OLDPWD']}/tungsten-diag-#{Time.now.localtime.strftime("%Y-%m-%d-%H-%M-%S")}"
       Timeout.timeout(5) {
@@ -72,7 +72,7 @@ module ClusterDiagnosticPackage
       error("Unable to use the #{diag_dir} directory because it already exists")
     end
     FileUtils.mkdir_p(diag_dir)
-    
+
     get_deployment_configurations().each{
       |config|
       build_topologies(config)
@@ -82,7 +82,7 @@ module ClusterDiagnosticPackage
       FileUtils.mkdir_p("#{diag_dir}/#{h_alias}")
       FileUtils.mkdir_p("#{diag_dir}/#{h_alias}/os_info")
       FileUtils.mkdir_p("#{diag_dir}/#{h_alias}/conf")
-      
+
       write_file("#{diag_dir}/#{h_alias}/manifest.json",@promotion_settings.getProperty([c_key, "manifest"]))
       write_file("#{diag_dir}/#{h_alias}/tpm.txt",@promotion_settings.getProperty([c_key, "tpm_reverse"]))
       write_file("#{diag_dir}/#{h_alias}/tpm_diff.txt",@promotion_settings.getProperty([c_key, "tpm_diff"]))
@@ -95,9 +95,9 @@ module ClusterDiagnosticPackage
 
       if @promotion_settings.getProperty([c_key, REPLICATOR_ENABLED]) == "true"
 
-      
+
         write_file("#{diag_dir}/#{h_alias}/trepctl.json", @promotion_settings.getProperty([c_key, "replicator_json_status"]))
-        
+
         out = File.open("#{diag_dir}/#{h_alias}/trepctl.txt", "w")
         config.getPropertyOr([REPL_SERVICES], {}).keys().sort().each{
           |rs_alias|
@@ -107,7 +107,7 @@ module ClusterDiagnosticPackage
           out.puts(@promotion_settings.getProperty([c_key, "replicator_status_#{rs_alias}"]))
         }
         out.close
-      
+
         out = File.open("#{diag_dir}/#{h_alias}/thl.txt", "w")
         config.getPropertyOr([REPL_SERVICES], {}).keys().sort().each{
           |rs_alias|
@@ -127,7 +127,7 @@ module ClusterDiagnosticPackage
           out.puts(@promotion_settings.getProperty([c_key, "thl_index_#{rs_alias}"]))
         }
         out.close
-        
+
         get_log(config,"#{config.getProperty(CURRENT_RELEASE_DIRECTORY)}/tungsten-replicator/log/trepsvc.log","#{diag_dir}/#{h_alias}/trepsvc.log")
         get_log(config,"#{config.getProperty(CURRENT_RELEASE_DIRECTORY)}/tungsten-replicator/log/xtrabackup.log", "#{diag_dir}/#{h_alias}/xtrabackup.log")
         get_log(config,"#{config.getProperty(CURRENT_RELEASE_DIRECTORY)}/tungsten-replicator/log/mysqldump.log", "#{diag_dir}/#{h_alias}/mysqldump.log")
@@ -175,7 +175,7 @@ module ClusterDiagnosticPackage
           end
         end
       end
-      
+
       if @promotion_settings.getProperty([c_key, CONNECTOR_ENABLED]) == "true"
         get_log(config,"#{config.getProperty(CURRENT_RELEASE_DIRECTORY)}/tungsten-connector/log/connector.log", "#{diag_dir}/#{h_alias}/connector.log" )
       end
@@ -198,28 +198,32 @@ module ClusterDiagnosticPackage
       end
 
       write_file("#{diag_dir}/#{h_alias}/os_info/ifconfig.txt",run_command(config,"ifconfig") )
-      write_file("#{diag_dir}/#{h_alias}/os_info/netstat.txt",run_command(config,"netstat -nap") )
+      write_file("#{diag_dir}/#{h_alias}/os_info/netstat.txt",run_command(config,"netstat -nap 2>&1") )
+      write_file("#{diag_dir}/#{h_alias}/os_info/netstat_sudo.txt",run_command(config,"sudo netstat -nap 2>&1") )
       write_file("#{diag_dir}/#{h_alias}/os_info/free.txt",run_command(config,"free -m") )
       write_file("#{diag_dir}/#{h_alias}/os_info/java_info.txt",run_command(config,"java -version 2>&1") )
       write_file("#{diag_dir}/#{h_alias}/os_info/ruby_info.txt",run_command(config,"ruby -v") )
       write_file("#{diag_dir}/#{h_alias}/os_info/uptime.txt",run_command(config,"uptime") )
+      write_file("#{diag_dir}/#{h_alias}/os_info/meminfo.txt",run_command(config,"cat /proc/meminfo") )
+      write_file("#{diag_dir}/#{h_alias}/os_info/cpuinfo.txt",run_command(config,"cat /proc/cpuinfo") )
+
 
     }
-    
+
     require 'zip/zip'
     require 'find'
-    
+
     @zip_file = "#{diag_dir}.zip"
     Zip::ZipFile.open(@zip_file, Zip::ZipFile::CREATE) do |zipfile|
       Find.find(diag_dir) do |path|
         entry = path.gsub(File.dirname(diag_dir) + "/", "")
         zipfile.add(entry, path)
       end
-      zipfile.close 
+      zipfile.close
     end
     FileUtils.rmtree(diag_dir)
   end
-  
+
   # Copy specified log's last n bytes.
   def copy_log(src_path, dest_path, bytes)
     if File.exist?(src_path)
@@ -241,11 +245,11 @@ end
 
 class ClusterDiagnosticCheck < ConfigureValidationCheck
   include CommitValidationCheck
-  
+
   def set_vars
     @title = "Collect diagnostic information"
   end
-  
+
   def validate
     c = Configurator.instance
     current_release_directory = @config.getProperty(CURRENT_RELEASE_DIRECTORY)
@@ -253,12 +257,12 @@ class ClusterDiagnosticCheck < ConfigureValidationCheck
     trepctl_cmd = c.get_trepctl_path(current_release_directory, @config.getProperty(REPL_RMI_PORT))
     thl_cmd = c.get_thl_path(current_release_directory)
     tpm_cmd = c.get_tpm_path(current_release_directory)
-    
+
     begin
       output_property("manifest", cmd_result("cat #{current_release_directory}/.manifest.json"))
       output_property("tpm_reverse", cmd_result("#{tpm_cmd} reverse --public"))
       output_property("tpm_diff", cmd_result("#{tpm_cmd} query modified-files"))
-      
+
       ["manager", "replicator", "connector"].each {
         |svc|
         svc_path = c.get_svc_path(svc, c.get_base_path())
@@ -267,7 +271,7 @@ class ClusterDiagnosticCheck < ConfigureValidationCheck
           cmd_result("#{svc_path} dump", true)
         end
       }
-      
+
       if c.svc_is_running?(c.get_svc_path("manager", c.get_base_path()))
         cmd_result("echo 'physical;*/*/manager/ServiceManager/diag' | #{cctrl_cmd} -expert", true)
         cmd_result("echo 'physical;*/*/router/RouterManager/diag' | #{cctrl_cmd} -expert", true)
@@ -276,10 +280,10 @@ class ClusterDiagnosticCheck < ConfigureValidationCheck
         output_property("cctrl_ping", cmd_result("echo 'ping ' | #{cctrl_cmd} -expert", true))
         output_property("cctrl_validate", cmd_result("echo 'cluster validate ' | #{cctrl_cmd} -expert", true))
       end
-      
+
       if c.svc_is_running?(c.get_svc_path("replicator", c.get_base_path()))
         output_property("replicator_json_status", cmd_result("#{trepctl_cmd} services -full -json", true))
-        
+
         @config.getPropertyOr([REPL_SERVICES], {}).keys().sort().each{
           |rs_alias|
           if rs_alias == DEFAULTS
@@ -295,7 +299,7 @@ class ClusterDiagnosticCheck < ConfigureValidationCheck
       error(ce.message)
     end
   end
-  
+
   def enabled?
     super() && (@config.getProperty(HOST_ENABLE_REPLICATOR) == "true")
   end
@@ -303,11 +307,11 @@ end
 
 class OldServicesRunningCheck < ConfigureValidationCheck
   include CommitValidationCheck
-  
+
   def set_vars
     @title = "Check for Tungsten services running outside of the current install directory"
   end
-  
+
   def validate
     current_release_directory = @config.getProperty(CURRENT_RELEASE_DIRECTORY)
     if File.exists?(current_release_directory)
@@ -315,7 +319,7 @@ class OldServicesRunningCheck < ConfigureValidationCheck
     else
       return
     end
-    
+
     current_pid_files = cmd_result("find #{@config.getProperty(HOME_DIRECTORY)}/#{RELEASES_DIRECTORY_NAME} -name *.pid").split("\n")
     allowed_pid_files = []
     if @config.getProperty(HOST_ENABLE_REPLICATOR) == "true"
@@ -327,7 +331,7 @@ class OldServicesRunningCheck < ConfigureValidationCheck
     if @config.getProperty(HOST_ENABLE_CONNECTOR) == "true"
       allowed_pid_files << "#{current_release_target_dir}/tungsten-connector/var/tconnector.pid"
     end
-    
+
     extra_pid_files = current_pid_files - allowed_pid_files
     extra_pid_files.each{
       |p|
